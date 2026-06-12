fb5118d1514f4a147ff102683a3507452cb9ea75
Vaibhav Kumbhar <vkumbhar94@gmail.com>
2026-06-12 23:47:52 +0530

    Simplify formula syntax - use if/else instead of nested on_* blocks for better compatibility

--- Formula/bitbucket-cli.rb
+++ Formula/bitbucket-cli.rb
@@ -3,36 +3,26 @@ class BitbucketCli < Formula
   homepage "https://github.com/vkumbhar94/bitbucket-cli"
   version "1.0.4"
 
-  on_macos do
-    on_arm64 do
+  if OS.mac?
+    if Hardware::CPU.arm?
       url "https://github.com/vkumbhar94/bitbucket-cli/releases/download/v1.0.4/bitbucket-cli-v1.0.4-darwin-arm64"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_ARM64"
-    end
-    on_x86_64 do
+    else
       url "https://github.com/vkumbhar94/bitbucket-cli/releases/download/v1.0.4/bitbucket-cli-v1.0.4-darwin-amd64"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_AMD64"
     end
-  end
-
-  on_linux do
-    on_arm64 do
+  elsif OS.linux?
+    if Hardware::CPU.arm?
       url "https://github.com/vkumbhar94/bitbucket-cli/releases/download/v1.0.4/bitbucket-cli-v1.0.4-linux-arm64"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_ARM64"
-    end
-    on_x86_64 do
+    else
       url "https://github.com/vkumbhar94/bitbucket-cli/releases/download/v1.0.4/bitbucket-cli-v1.0.4-linux-amd64"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_AMD64"
     end
   end
 
   def install
-    # Find the binary file that was downloaded and install it
-    binary = Dir.glob("bitbucket-cli-*-*-*").first
-    if binary
-      bin.install binary => "bitbucket-cli"
-    else
-      raise "Could not find bitbucket-cli binary"
-    end
+    bin.install "bitbucket-cli-v1.0.4-#{OS.kernel_name.downcase}-#{Hardware::CPU.arch}" => "bitbucket-cli"
   end
 
   test do
