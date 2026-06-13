b1a04b781894dc0af2774372e790c46310298918
Vaibhav Kumbhar <vkumbhar94@gmail.com>
2026-06-13 22:20:55 +0530

    Update checksum for v1.0.4 release

--- Formula/bitbucket-cli.rb
+++ Formula/bitbucket-cli.rb
@@ -2,7 +2,7 @@ class BitbucketCli < Formula
   desc "CLI tool for Bitbucket Server to manage pull requests and integrate with Cursor"
   homepage "https://github.com/vkumbhar94/bitbucket-cli"
   url "https://github.com/vkumbhar94/bitbucket-cli/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "8d5e7f5f5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e5e"
+  sha256 "e31aa5c8581d4cc6ed6838352539a8373fcfb11f799374aec273e8f514ddc77d"
   version "1.0.4"
   license "MIT"
 
